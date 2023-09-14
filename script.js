cd8b993bc0f4807064496344c068c61821dbfe06
kabir shuvo <kabirshuvo19@gmail.com>
2023-09-14 13:18:11 +0600

    Updated

--- script.js
+++ script.js
@@ -1,58 +1,92 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, 'red');
+gradient.addColorStop(0.2, 'yellow');
+gradient.addColorStop(0.4, 'green');
+gradient.addColorStop(0.6, 'cyan');
+gradient.addColorStop(0.8, 'blue');
+gradient.addColorStop(1, 'magenta');
 
 class Symbol {
-    constructor(x, y, fontSize, canvasHeight){
-        this.characters = `
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters = `
         アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン
         0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ ♔♕♖♗♘♙CHESS♚♛♜♝♞♟ ☀☁❆WEATHER❅❄ ♪MUSIC♫`;
-        this.x = x;
-        this.y = y;
-        this.fontSize = fontSize;
-        this.canvasHeight = canvasHeight;
-
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.canvasHeight = canvasHeight;
+  }
+  draw(context) {
+    this.text = this.characters.charAt(
+      Math.floor(Math.random() * this.characters.length)
+    );
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.99) {
+      this.y = 0;
+    } else {
+      this.y += 1;
     }
-    draw(context){
-        this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
-        context.fillStyle = '#0aff0a';
-        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if(this.y * this.fontSize > this.canvasHeight){
-            this.y = 0;
-        }else {
-            this.y += 1;
-        }
-    }
+  }
 }
 
-
 class Effect {
-    constructor(canvasWidth, canvasHeight){
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
-        this.fontSize = 25;
-        this.columns = this.canvasWidth/this.fontSize;
-        this.symbols = [];
-        this.#initialize();
-        console.log(this.symbols);
-    }
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 19;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+    console.log(this.symbols);
+  }
 
-    #initialize(){
-        for (let i = 0; i <this.columns; i++){
-            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
-
-        }
+  #initialize() {
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
+  }
+  resize(width, height){
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth/this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+  }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 1;
+const nextFrame = 1000 / fps;
+let timer = 0;
 
-function animate(){
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
-    requestAnimationFrame(animate);
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.07)";
+    ctx.textAlign = 'center';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = gradient;
+    // "#0aff0a";
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+  }else {
+    timer += deltaTime;
+  }
+
+  requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
+
+window.addEventListener('resize', function(){
+    canvas.width = this.window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+})
