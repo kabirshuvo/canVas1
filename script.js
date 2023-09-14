4e50c87b1bfde05a48f82346cfd7b4b877971663
kabir shuvo <kabirshuvo19@gmail.com>
2023-09-14 09:38:27 +0600

    I got my symbols array in console and it is working as expected

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ class Symbol {
     constructor(x, y, fontSize, canvasHeight){
         this.characters = `
         アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン
-        0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ ♔♕♖♗♘♙CHESS♚♛♜♝♞♟ ☀☁❆WEATHER❅❄ ♪MUSIC♫`
+        0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ ♔♕♖♗♘♙CHESS♚♛♜♝♞♟ ☀☁❆WEATHER❅❄ ♪MUSIC♫`;
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.canvasHeight = canvasHeight;
 
     }
-    draw(){
-
+    draw(context){
+        this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
+        context.fillStyle = '#0aff0a';
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        if(this.y * this.fontSize > this.canvasHeight){
+            this.y = 0;
+        }else {
+            this.y += 1;
+        }
     }
 }
 
@@ -24,12 +35,24 @@ class Effect {
         this.fontSize = 25;
         this.columns = this.canvasWidth/this.fontSize;
         this.symbols = [];
+        this.#initialize();
+        console.log(this.symbols);
     }
 
     #initialize(){
-        for (let i = 0; i <this.columns.length; i++){
-            this.symbols[i] = new Symbol();
+        for (let i = 0; i <this.columns; i++){
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+
         }
     }
 }
 
+const effect = new Effect(canvas.width, canvas.height);
+
+function animate(){
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    requestAnimationFrame(animate);
+}
+
+animate();
